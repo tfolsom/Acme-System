239bacd36c61b9ce421ecd4293471309eae88c63
Trennt Folsom <trenntfolsom@gmail.com>
2023-08-26 11:29:06 -0400

    Tutorial 11, Step 6

--- ACME-System/src/ver1/HelloWorld.java
+++ ACME-System/src/ver1/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello From Eclipse");
-
+		System.out.println("Trennt is working locally on his branch");
 	}
 
 }
